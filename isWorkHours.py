d3c79f2e7eada07bc962fcb35cb3f998a800f453
Rani Murali <75182997+ranimurali412@users.noreply.github.com>
2021-04-22 16:35:56 -0400

    attempt to convert isWorkHours

---
+++ isWorkHours.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# ISWORKHOURS Outputs true if t is between 9am and 5pm on a weekday
+#   t is day of the year, we assume that the year starts on a monday
+
+def isWorkHours(t):
+    w = np.zeros(t)
+    w([mod(floor(t),7)<6 & (mod(t,1) > 0.375 & mod(t,1) < 0.7083)]) = 1
+    return w
