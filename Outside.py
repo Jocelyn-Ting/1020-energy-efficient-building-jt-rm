823c451b569d0019b399d9e022a0f01dc5453143
jhting <jhting@mit.edu>
2021-04-24 16:38:08 -0400

    converted Outside

--- Outside.py
+++ Outside.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from CtoK import CtoK
 
 class Outside:
     #Outside Simple class to store outside temperatures and other data
@@ -10,15 +11,16 @@ class Outside:
     #   Outside.T_sky gives sky temperature at current time stamp
     #   Outside.S gives solar radiation levels at current time stamp  
     def __init__(self,T, T_sky, S):
-        self.T = T
-        self.T_sky = T_sky
-        self.S = S
+        self.weather_data = pd.read_csv('cambridge_weather.csv')
         
-    def Outside(self,t,S, T, T_sky):
-        with open('cambridge_weather.csv', newline='') as csvfile:
-            weather_data = list(csv.reader(csvfile))
-        outside.S = lambda t: interp1(weather_data[:,1],weather_data[:,2],t)
-        outside.T = lambda t: interp1(weather_data[:,1],CtoK(weather_data[:,3]),t)
-        outside.T_sky = lambda t: interp1(weather_data[:,1],CtoK(weather_data[:,4]),t)
+    @property    
+    def S(self,t):
+        return np.interp(t, self.weather_data.iloc[:,0],self.weather_data.iloc[:,1])
+    @property 
+    def T(self,t):
+        return np.interp(t, self.weather_data.iloc[:,0],CtoK(self.weather_data.iloc[:,2]))
+    @property 
+    def T_sky(self,t):   
+        return np.interp(t, self.weather_data.iloc[:,0],CtoK(self.weather_data.iloc[:,3]))
 
 
